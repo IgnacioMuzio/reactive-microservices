c8f63061307c785efbfe2e0f8b494ca6988a2bb8
Ignacio Muzio <fmuzio@mobydigital.com>
2024-07-22 16:54:28 -0300

    ms-names created

---
+++ ms-names/src/main/java/com/im/ms_names/MsNamesApplication.java
@@ -0,0 +1,13 @@
+package com.im.ms_names;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNamesApplication.class, args);
+	}
+
+}
